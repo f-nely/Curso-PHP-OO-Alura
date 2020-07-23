6737c00f5c3d310683a8170d1d56981f03921276
nely de faria <nelly.nf54@gmail.com>
2020-07-23 19:16:45 -0300

    Adiciona classe Funcionário

---
+++ project/src/Modelo/Funcionario/Funcionario.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Alura\Banco\Modelo\Funcionario;
+use Alura\Banco\Modelo\Cpf;
+use Alura\Banco\Modelo\Pessoa;
+
+abstract class Funcionario extends Pessoa
+{
+    private float $salario;
+
+    public function __construct(string $nome, Cpf $cpf, float $salario)
+    {
+        parent::__construct($nome,$cpf);
+        $this->validaNome($nome);
+        $this->salario = $salario;
+    }
+
+    public function alteraNome($nome): void
+    {
+        $this->validaNome($nome);
+        $this->nome->$nome;
+    }
+
+    public function recebeAumento(float $valorAumento): void
+    {
+        if ($valorAumento < 0) {
+            echo "Aumento deve ser positivo";
+            return;
+        }
+
+        $this->salario += $valorAumento;
+    }
+
+    public function recuperaSalario()
+    {
+        return $this->salario;
+    }
+
+    abstract public function calculaBonificacao(): float;
+
+}

6e6f4b5b2a4123da4d09376c3a89aa421c4d8af5
nely de faria <nelly.nf54@gmail.com>
2020-07-23 19:01:20 -0300

    Adiciona classe Titular

---
+++ project/src/Modelo/Conta/Titular.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Alura\Banco\Modelo\Conta;
+
+use Alura\Banco\Modelo\Autenticavel;
+use Alura\Banco\Modelo\CPF;
+use Alura\Banco\Modelo\Endereco;
+use Alura\Banco\Modelo\Pessoa;
+
+class Titular extends Pessoa implements Autenticavel
+{
+    private Endereco $endereco;
+
+    public function __construct(Cpf $cpf, string $nome, Endereco $endereco)
+    {
+        parent::__construct($nome,$cpf);
+        $this->validaNome($nome);
+        $this->endereco = $endereco;
+    }
+
+    public function recuperaEndereco(): Endereco
+    {
+        $this->endereco;
+    }
+
+    public function podeAutenticar(string $senha): bool
+    {
+        return $senha === 'abcd';
+    }
+}

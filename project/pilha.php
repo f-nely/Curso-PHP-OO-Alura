fe6ee1bc9d349602e1aeb5cf24eee2de040d3966
nely de faria <nelly.nf54@gmail.com>
2020-08-02 13:04:24 -0300

    Alterando try catch

--- project/pilha.php
+++ project/pilha.php
@@ -4,19 +4,22 @@ function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
     try {
-        $arrayFixo = new SplFixedArray(2);
-        $arrayFixo[3] = 'Valor';
+        funcao2();
     } catch (RuntimeException $problem) {
-        echo "Aconteceu um erro na função 1" . PHP_EOL;
+        echo "Na função 1, eu resolvi o problema da função 2" . PHP_EOL;
     }
-    //$divisao = intdiv(5, 0);
-    funcao2();
+
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 'Valor';
+    //$divisao = intdiv(5, 0);
+
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }

7a4789654139da360fd09fd6874689d11321654a
nely de faria <nelly.nf54@gmail.com>
2020-08-02 13:23:02 -0300

    Usando Multi-catch

--- project/pilha.php
+++ project/pilha.php
@@ -5,10 +5,8 @@ function funcao1()
     echo 'Entrei na função 1' . PHP_EOL;
     try {
         funcao2();
-    } catch (RuntimeException $problem) {
+    } catch (RuntimeException | DivisionByZeroError $problem) {
         echo "Na função 1, eu resolvi o problema da função 2" . PHP_EOL;
-    } catch (DivisionByZeroError $error) {
-        echo "Erro ao dividir um número por 0" . PHP_EOL;
     }
 
     echo 'Saindo da função 1' . PHP_EOL;
@@ -18,10 +16,10 @@ function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
 
-    $divisao = intdiv(5, 0);
+
     $arrayFixo = new SplFixedArray(2);
     $arrayFixo[3] = 'Valor';
-
+    $divisao = intdiv(5, 0);
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }

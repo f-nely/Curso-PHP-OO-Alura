0db8726797e715850277a51885c6209f90ea79a4
nely de faria <nelly.nf54@gmail.com>
2020-08-03 17:33:53 -0300

    Tratando exceção na função saca

--- project/teste-saque.php
+++ project/teste-saque.php
@@ -1,18 +1,25 @@
 <?php
 
-use Alura\Banco\Modelo\Conta\{ContaPoupanca, ContaCorrente, Titular};
-use Alura\Banco\Modelo\{CPF, Endereco};
+use Alura\Banco\Modelo\Conta\{ContaPoupanca, ContaCorrente, SaldoInsufucienteException, Titular};
+use Alura\Banco\Modelo\{Cpf, Endereco};
 
 require_once 'autoload.php';
 
 $conta = new ContaPoupanca(
     new Titular(
-        new CPF('123.456.789-10'),
+        new Cpf('123.456.789-10'),
         'Vinicius Dias',
         new Endereco('Petrópolis', 'bairro Teste', 'Rua lá', '37')
     )
 );
 $conta->deposita(500);
-$conta->saca(100);
+
+try {
+    $conta->saca(700);
+} catch (SaldoInsufucienteException $exception) {
+    echo "Você, não tem saldo para realizar este saque. " . PHP_EOL;
+    echo $exception->getMessage() . PHP_EOL;
+}
+
 
 echo $conta->recuperaSaldo();
